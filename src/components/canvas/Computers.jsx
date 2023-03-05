b4ecb4b0fbe781b250d0af99e9359135169adf02
junaid <onlyjunaid2002@gmail.com>
2023-03-06 00:19:53 +0530

    Fdscx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -34,23 +34,26 @@ const ComputersCanvas = () => {
 
   useEffect(() => {
     // Add a listener for changes to the screen size
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    // const mediaQuery = window.matchMedia("(max-width: 500px)");
 
-    // Set the initial value of the `isMobile` state variable
-    setIsMobile(mediaQuery.matches);
+    // // Set the initial value of the `isMobile` state variable
+    // setIsMobile(mediaQuery.matches);
 
-    // Define a callback function to handle changes to the media query
-    const handleMediaQueryChange = (event) => {
-      setIsMobile(event.matches);
-    };
+    // // Define a callback function to handle changes to the media query
+    // const handleMediaQueryChange = (event) => {
+    //   setIsMobile(event.matches);
+    // };
 
-    // Add the callback function as a listener for changes to the media query
-    mediaQuery.addEventListener("change", handleMediaQueryChange);
+    // // Add the callback function as a listener for changes to the media query
+    // mediaQuery.addEventListener("change", handleMediaQueryChange);
 
-    // Remove the listener when the component is unmounted
-    return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
-    };
+    // // Remove the listener when the component is unmounted
+    // return () => {
+    //   mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    // };
+    if (window.innerWidth < 500) {
+      setIsMobile(true);  
+    }
   }, []);
 
   return (
