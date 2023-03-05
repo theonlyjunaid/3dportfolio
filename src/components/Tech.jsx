a975cd8c74eca7ccb7cead65710c479a3ff9d0b1
junaid <onlyjunaid2002@gmail.com>
2023-03-06 00:31:42 +0530

    Fedcs

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -7,11 +7,17 @@ import { technologies } from "../constants";
 const Tech = () => {
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
+      {
+        window.innerWidth > 768 ? technologies.map((technology) => (
+      <div className='w-28 h-28' key={technology.name}>
+        <BallCanvas icon={technology.icon} />
+      </div>
+        )) : technologies.slice(4,10).map((technology) => (
+          <div className='w-28 h-28' key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))
+      }
     </div>
   );
 };
